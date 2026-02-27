82570d1bd7ff991f58d39226c2893ca3c79fd79d
Mazx1 <111648060+Mazx1@users.noreply.github.com>
2026-02-27 13:27:45 -0500

    mas
    Mas

--- FRONTEND/crud-productos/src/components/ProductList.jsx
+++ FRONTEND/crud-productos/src/components/ProductList.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { getProducts } from '../api/products'
 
 export default function ProductList() {
+
+    const loadProducts = async() => {
+        const response = await getProducts()
+        console.log(response)
+    }
+    loadProducts()
+
   return (
     <div>
       producto lista
