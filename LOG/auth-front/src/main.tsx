4c913b884dab6ea5998b8c1613a30599685646c8
Mazx1 <111648060+Mazx1@users.noreply.github.com>
2026-03-09 16:19:59 -0500

    ya areglamos rutas basicas

--- LOG/auth-front/src/main.tsx
+++ LOG/auth-front/src/main.tsx
@@ -1,10 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+//pagina de inicio
 import App from './App.tsx'
+//pagina de rutas
+import Login from './routes/login.tsx'
+import Signup from './routes/signup.tsx'
+import Dashboard from './routes/Dashboard.tsx'
+
+//las rutas 
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+
+
+
+
+// vamod a crear las rutas
+const router = createBrowserRouter([
+  {
+      path: '/',
+     element: <App />,
+    },
+     {
+      path: '/login',
+     element: <Login />,
+    },
+    {
+      path: '/signup',
+     element: <Signup/>,
+    },
+    {
+      path: '/dashboard',
+     element: <Dashboard />,
+    }
+])
+  
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
