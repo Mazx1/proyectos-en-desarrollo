a56a434e238db55dd1d8ad13eb7c0c582f6a7048
Mazx1 <111648060+Mazx1@users.noreply.github.com>
2026-06-18 17:52:27 -0500

    solo hace falta los botones y las animaciones y terminamos

--- tarjeta/mi-proyecto-react/src/App.jsx
+++ tarjeta/mi-proyecto-react/src/App.jsx
@@ -30,7 +30,15 @@ function App() {
       <section className="hero">
         <h1>MIS XV</h1>
         <h2>GABRIELA</h2>
-        <p>Con mucho amor te invito a celebrar este momento especial</p>
+      
+      </section>
+      <section className="hero1">
+        
+        <p>
+          Con mucho amor te invito a celebrar este momento muuy especial en mi vida,
+          espero que puedas acompañarme a disfrutar de este día tan importante para mí.
+          Sera una noche magica e inolvidable, llena de sorpresas y momentos que quedaran en nuestros corazones para siempre.
+          </p>
       </section>
 
       {/* 📜 SCROLL CONTENIDO */}
@@ -41,20 +49,55 @@ function App() {
         </div>
 
         <div className="scroll-content">
-          <img src="/imagen.jpg" alt="imagen" />
+          <img src="/imagen4.jpg" alt="imagen4" />
         </div>
 
         <div className="scroll-content">
-          <img src="/imagen.jpg" alt="imagen" />
+          <img src="/imagen3.jpg" alt="imagen3" />
         </div>
 
         <div className="scroll-content">
-          <img src="/imagen.jpg" alt="imagen" />
-        </div>
+  <img src="/imagen.jpg" alt="imagen" />
+  
+  <a href="https://wa.me/573106973754"
+    target="_blank"
+    rel="noreferrer"
+    className="whatsapp-btn"
+  >
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+      alt="WhatsApp"
+      style={{ width: "24px", height: "24px" }}
+    />
+    Confirmar asistencia por WhatsApp
+  </a>
+</div>
 
         <div className="scroll-content">
-          <img src="/imagen.jpg" alt="imagen" />
+          <div className="texto">
+            
+          </div>
+            
         </div>
+        <section className="ubicacion">
+        <h3>¿Cómo llegar?</h3>
+        <div className="mapa-card">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.9809448869737!2d-74.09138112432362!3d4.773242841023001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f843a756f8687%3A0x7f26fd52db7fc59f!2sBogota%20Sport%20Club%2C%20Suba%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1781821351907!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            width="100%"
+            height="300"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+          <div className="mapa-info">
+            <p className="mapa-nombre">Bogota Sport Club</p>
+            <p className="mapa-direccion"> Avenida Suba - Cota, Kilómetro 4</p>               
+            
+          </div>
+        </div>
+      </section>
 
       </div>
 
